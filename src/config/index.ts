2522545d665cd4bbaf66f95012a004401952259e
Leon <82466943+LeonTheDev-io@users.noreply.github.com>
2022-02-11 18:40:27 +0100

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,8 @@ import type { AppI18nNamespaces } from 'react-i18next';
 import localeConfig from '../../public/locales/config.json';
 
 export const SITE_TITLE = 'HammerTime';
-export const CANONICAL_URL = 'https://hammertime.djdavid98.art';
-export const REPOSITORY_URL = 'https://github.com/DJDavid98/HammerTime';
+export const CANONICAL_URL = 'https://discord-hammertime.netlify.app/';
+export const REPOSITORY_URL = 'https://github.com/LeonTheDev-io/HammerTime';
 export const FONTAWESOME_FREE_LICENSE_URL = 'https://fontawesome.com/license/free';
 
 export const IS_CLIENT_SIDE = typeof window !== 'undefined';
